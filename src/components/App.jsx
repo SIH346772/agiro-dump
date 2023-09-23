555914ef27bd083f05ce7ba974a4732063f3b3fe
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-09-24 00:04:29 +0530

    chore: tailwindcss and flowbite added

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Just a test</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import flowbite from "flowbite/plugin";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite],
+};
